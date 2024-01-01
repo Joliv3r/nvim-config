31bc58594c3a7911bda41a336d1943c46cde26a3
Joliver <jacobbruun@hotmail.no>
2024-01-01 15:17:57 +0100

    Added gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -86,6 +86,9 @@ return packer.startup(function(use)
     config = function() require("nvim-autopairs").setup {} end
   }
 
+  -- Gitsigns
+  use "lewis6991/gitsigns.nvim"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
