e82eaa870552c23edf00a4fe5e47ebb0d527434c
Joliver <jacobbruun@hotmail.no>
2024-01-04 13:50:24 +0100

    Some formating

--- lua/user/rainbow.lua
+++ lua/user/rainbow.lua
@@ -9,27 +9,27 @@ function M.config()
 
   ---@type rainbow_delimiters.config
   vim.g.rainbow_delimiters = {
-      strategy = {
-          [''] = rainbow_delimiters.strategy['global'],
-          vim = rainbow_delimiters.strategy['local'],
-      },
-      query = {
-          [''] = 'rainbow-delimiters',
-          lua = 'rainbow-blocks',
-      },
-      priority = {
-          [''] = 110,
-          lua = 210,
-      },
-      highlight = {
-          'RainbowDelimiterRed',
-          'RainbowDelimiterYellow',
-          'RainbowDelimiterBlue',
-          'RainbowDelimiterOrange',
-          'RainbowDelimiterGreen',
-          'RainbowDelimiterViolet',
-          'RainbowDelimiterCyan',
-      },
+    strategy = {
+      [''] = rainbow_delimiters.strategy['global'],
+      vim = rainbow_delimiters.strategy['local'],
+    },
+    query = {
+      [''] = 'rainbow-delimiters',
+      lua = 'rainbow-blocks',
+    },
+    priority = {
+      [''] = 110,
+      lua = 210,
+    },
+    highlight = {
+      'RainbowDelimiterRed',
+      'RainbowDelimiterYellow',
+      'RainbowDelimiterBlue',
+      'RainbowDelimiterOrange',
+      'RainbowDelimiterGreen',
+      'RainbowDelimiterViolet',
+      'RainbowDelimiterCyan',
+    },
   }
 end
 
