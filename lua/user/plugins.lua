17a4e60f2feac6e95439e61a26202e77d0e1d636
Joliver <jacobbruun@hotmail.no>
2024-01-04 15:27:10 +0100

    Added alpha and which-key through spec

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -11,47 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-
-  -- Colorschemes
-  -- "lunarvim/colorschemes",  -- Some extra colorschemes
-
-  -- cmp plugins
-
-  -- snippets
-
-  -- LSP
-
-  -- Telescope
-  --  "nvim-telescope/telescope-media-files.nvim"  -- Telescope extension
-
-  -- Treesitter
-
-  -- Autopairs
-
-  -- Gitsigns
-
-  -- nvim-tree
-
-  -- Bufferline
-
-  -- Starting page
-  {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-  },
-
-  -- Help for keybindings
-  {"folke/which-key.nvim", event = 'VimEnter'},
-
-  -- Toggleterm
-}
-
-for _, v in pairs(plugins) do
-  table.insert(LAZY_PLUGIN_SPEC, v)
-end
 
 require("lazy").setup {
   spec = LAZY_PLUGIN_SPEC,
